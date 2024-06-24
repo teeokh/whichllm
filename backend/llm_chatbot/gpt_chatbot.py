f890e35b1d41fed93fda7322db0985060724ced8
Tee <139244119+teeokh@users.noreply.github.com>
2024-06-24 14:59:22 +0100

    Move get_secret to gpt_chatbot

--- backend/llm_chatbot/gpt_chatbot.py
+++ backend/llm_chatbot/gpt_chatbot.py
@@ -1,5 +1,33 @@
 from openai import OpenAI
-from getsecret import get_secret
+import os
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret(secret_name):
+
+    secret_name = secret_name
+    region_name = "eu-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+
+    except ClientError as e:
+        raise e
+
+
+    secret = get_secret_value_response['SecretString']
+    secret=json.loads(secret)
+    for key, value in secret.items():os.environ[key] = value
 
 openai_api_key = get_secret("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
