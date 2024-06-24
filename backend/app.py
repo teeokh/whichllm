337a5e6957661f2a14f11d157b04d3cc495a144e
Tee <139244119+teeokh@users.noreply.github.com>
2024-06-24 18:05:26 +0100

    Change backend port to 5000

--- backend/app.py
+++ backend/app.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all() # Create all the models we have to find in our database - if we don't have the database created already
 
-    app.run(port=8080)
+    app.run(port=5000)
